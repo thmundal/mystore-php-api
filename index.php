8e05a3e6b2ab2b9b30a5c003525d465903a21faf
Thomas Mundal <thmundal@gmail.com>
2015-05-08 09:09:43 +0200

    Updated example file

--- index.php
+++ index.php
@@ -1,11 +1,54 @@
 <?php
+header("content-type:text/html");
 require_once("mystore.php");
+require_once("../Util/util.php");
 
 $mystore = new MystoreApi("NIVO-45C882B8-42");
+?><!DOCTYPE html>
+<html>
+<body>
 
-$r = $mystore->api("get_categories", ["prosduct_data" => ["name" =>"blah"]]);
+<div>
+<a href="?a=list_products">List products</a>|<a href="?a=list_categories">List categories</a>|<a href="?a=csv_import">CSV import</a>
+</div>
 
-echo "<pre>";
-var_dump($r);
-echo "</pre>";
+
+<?php
+
+$r = $mystore->api("get_all_products");
+
+
+
+
+switch(arrGet($_GET, 'a', false)) {
+	case 'list_products':
+		$a_product = $r->data->product_data->{"132"};
+
+		//var_dump($a_product);
+
+		foreach($a_product as $field => $value) {
+			pre_print_r($field);
+		}
+	break;
+
+	case 'list_categories':
+	break;
+
+	case 'csv_import':
+		?>
+		<form name="csv" method="post" type="multipart/form-data">
+		<table>
+		<tr><td>CSV File</td><td><input type="file" name="file" /></td></tr>
+		</table>
+		</form>
+		<?php
+	break;
+
+	default:
+	echo "blah";
+	break;
+}
 ?>
+
+</body>
+</html>
